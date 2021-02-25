01c382c352d794670ec1453603b4605034993017
Ayushi Gurjar <78641391+ayu-g@users.noreply.github.com>
2021-02-26 00:28:31 +0530

    Add files via upload

---
+++ WELLCOME.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class FDemo extends JFrame implements ActionListener
+{
+    JButton b1,b2;
+    FirstPage fp;
+    About ab ;
+    Login log;
+    Container cn = getContentPane();
+    CardLayout card;
+    FDemo ()
+    {
+        
+         card = new CardLayout();
+         setLayout(card);
+
+        fp =new FirstPage(this);
+        cn.add("FirstPage",fp);
+
+
+        ab =new About(this);
+        cn.add("About",ab);
+
+        log = new Login(this);
+        cn.add("Login",log);
+        
+        
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==fp.b1)
+        {
+            card.show(cn,"About");
+        }
+        if(e.getSource()==fp.b2)
+        {
+            card.show(cn,"Login");
+        }
+        if(e.getSource()==ab.b1)
+        {
+            card.show(cn,"FirstPage");
+        }
+        if(e.getSource()==log.b2)
+        {
+            card.show(cn,"FirstPage");
+        }
+    }
+
+}
+class WELLCOME
+{
+    public static void main(String ar[])
+    {
+        FDemo f = new FDemo();
+        f.setVisible(true);
+        f.setBounds(100,100,700,500);
+        f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+    }
+}
